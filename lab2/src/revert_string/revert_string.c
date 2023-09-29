3b4a6ff13c11db0406215fa3abb6775aab03137a
MrMD <58086007+MrMirDan@users.noreply.github.com>
2023-09-29 18:39:02 +0000

    Update 8 files

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,18 @@
 #include "revert_string.h"
+#include <stdio.h>
 
 void RevertString(char *str)
 {
-	// your code here
+	int size = sizeof(str)/sizeof(char)+1;
+	printf("%d",sizeof(str));
+	printf("%d",sizeof(char));
+	printf("%d\n",size);
+	int i = 0;
+	while(i <= size/2){
+		char buff = *(str+i);
+		*(str+i) = *(str+size-i);
+		*(str+size-i) = buff;
+		++i;
+	}
 }
 
