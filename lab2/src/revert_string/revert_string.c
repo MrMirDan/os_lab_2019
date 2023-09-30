0411464f769de59a34d88418c5a80abec12d1ac9
MrMD <58086007+MrMirDan@users.noreply.github.com>
2023-09-30 11:17:06 +0000

    Update 3 files

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,10 +1,12 @@
 #include "revert_string.h"
+#include <stdio.h>
 void RevertString(char *str)
 {
 	int len = 0;
 	for(int i=0; *(str+i) != '\0'; ++i){
 		++len;
 	}
+	printf("%d", len);
 	for(int i=0; i < len/2; ++i){
 		char buff = *(str+i);
 		*(str+i) = *(str+len-1-i);
